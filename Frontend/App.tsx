50c884047a704754a33017a1130a825b737cc3f9
AizenSousuke <puteranikdimas@yahoo.com.sg>
2025-03-10 22:03:31 +0800

    Updated with comments

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -22,6 +22,8 @@ import { useSelector } from "react-redux";
 const Stack = createStackNavigator();
 
 const App = () => {
+	// State is coming from Redux store
+	// Accesses state.home.token, which means your Redux store has a home slice that contains token.
 	let authToken = useSelector((state) => state.home.token);
 	let goingOut = useSelector((state) => state.busStop.GoingOut);
 	let goingHome = useSelector((state) => state.busStop.GoingHome);
--- Frontend/app/context/AuthContext.js
+++ Frontend/app/context/AuthContext.js
@@ -3,7 +3,13 @@ import React, { createContext } from "react";
 const { Provider, Consumer } = createContext();
 
 const AuthProvider = (props) => {
-	return <Provider value={{ token: props.value, updateToken: props.updateToken }}>{props.children}</Provider>;
+	return (
+		<Provider
+			value={{ token: props.value, updateToken: props.updateToken }}
+		>
+			{props.children}
+		</Provider>
+	);
 };
 
 export { AuthProvider };
